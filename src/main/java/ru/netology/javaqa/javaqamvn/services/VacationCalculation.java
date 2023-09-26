523a3d1f2d443b9b2114fa467ccdd3cdadb8bcc4
Olga <Taylakova_Olga@mail.ru>
2023-09-26 23:09:27 +0700

    first commit

---
+++ src/main/java/ru/netology/javaqa/javaqamvn/services/VacationCalculation.java
@@ -0,0 +1,22 @@
+
+package ru.netology.javaqa.javaqamvn.services;
+
+public class VacationCalculation {
+
+    public int calculate(int income, int expenses, int threshold) {
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
+        for (int month = 0; month < 12; month++) {
+            System.out.println(money);
+            if (money >= threshold) { // можем ли отдыхать?
+                count++; // увеличиваем счётчик месяцев отдыха
+                money = money - expenses;
+                money = money / 3;
+            } else {
+                money = money + income;
+                money = money - expenses;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/VacationCalculationTest.java
@@ -0,0 +1,22 @@
+
+package ru.netology.javaqa.javaqamvn.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+public class VacationCalculationTest {
+    @ParameterizedTest
+    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    })
+
+//    @CsvFileSource(files = "src/test/resources/vacation.csv")
+    public void testRegisteredUnderLimit(int expected, int income, int expenses, int threshold) {
+        VacationCalculation service = new VacationCalculation();
+        int actual = service.calculate(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
+    }
+}
